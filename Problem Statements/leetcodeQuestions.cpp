af9ca230bffdb7b78107f31f68c83a8ebe5ddabe
ankit-khatkar <ankitofficial1504@gmail.com>
2025-12-27 02:57:52 +0530

    add leetcode questions.

--- Problem Statements/leetcodeQuestions.cpp
+++ Problem Statements/leetcodeQuestions.cpp
@@ -414,9 +414,30 @@ int singleNumber(vector<int> &nums)
   return ans;
 }
 
+// QUE:- Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+int subarray(vector<int> &arr, int k)
+{
+  int n = arr.size();
+  int subarrays = 0;
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i; j < n; j++)
+    {
+      sum += arr[j];
+      if (sum == k)
+      {
+        subarrays += 1;
+      }
+    }
+    sum = 0;
+  }
+  return subarrays;
+}
 int main()
 {
-  vector<int> arr = {4, 1, 2, 1, 2};
-  singleNumber(arr);
+  vector<int> arr = {0, 0, 0};
+  cout << subarray(arr, 0);
   return 0;
 }
